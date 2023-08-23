c712c0ded9f9b5f3b31fe791837243e40f27af13
Agustín Alejandro Mota Hinojosa <2030406@upv.edu.mx>
2023-08-23 02:43:13 -0600

    clases anidadas hacen que los resultados sean totalmente diferentes

--- src/main/java/edu/upvictoria/fpoo/App.java
+++ src/main/java/edu/upvictoria/fpoo/App.java
@@ -2,24 +2,32 @@ package edu.upvictoria.fpoo;
 
 public class App
 {
-    public static void main( String[] args ) {
-        DirectedGraph<Integer> directedGraph = new DirectedGraph<>();
+    public static void main(String[] args) {
+        // Crear una instancia de DirectedGraph
+        var directedGraph = new Graph.DirectedGraph();
+
+        // Crear una instancia de UndirectedGraph
+        var undirectedGraph = new Graph.UndirectedGraph();
+
+        // Ahora puedes usar estas instancias para trabajar con los grafos dirigidos y no dirigidos.
+
+        // Ejemplo de cómo agregar vértices y aristas a un grafo dirigido:
         directedGraph.addVertex(1);
         directedGraph.addVertex(2);
         directedGraph.addVertex(3);
         directedGraph.addEdge(1, 2);
         directedGraph.addEdge(2, 3);
-        System.out.println("Grafo Dirigido:");
-        directedGraph.printGraph();
 
-        Graph<Integer> undirectedGraph = new UndirectedGraph<>();
+        // Ejemplo de cómo agregar vértices y aristas a un grafo no dirigido:
         undirectedGraph.addVertex(1);
         undirectedGraph.addVertex(2);
         undirectedGraph.addVertex(3);
         undirectedGraph.addEdge(1, 2);
-        undirectedGraph.addEdge(2, 3);
-        System.out.println("Grafo No Dirigido:");
-        undirectedGraph.printGraph();
+        directedGraph.addEdge(2, 3);
 
+        System.out.println("dirigido:");
+        directedGraph.printGraph();
+        System.out.println("no dirigido:");
+        undirectedGraph.printGraph();
     }
 }
--- src/main/java/edu/upvictoria/fpoo/Graph.java
+++ src/main/java/edu/upvictoria/fpoo/Graph.java
@@ -25,4 +25,79 @@ public abstract class Graph<T> implements GraphUtils<T> {
         return vertices.containsKey(vertex);
     }
 
+
+    public static class DirectedGraph<T> extends Graph<T> {
+
+        public DirectedGraph() {
+            super();
+        }
+
+        @Override
+        public void addVertex(T vertex) {
+            if (!vertices.containsKey(vertex)) {
+                vertices.put(vertex, new ArrayList<>());
+            }
+        }
+
+        @Override
+        public void addEdge(T sourceVertex, T destinationVertex) {
+            if (vertices.containsKey(sourceVertex) && vertices.containsKey(destinationVertex)) {
+                vertices.get(sourceVertex).add(destinationVertex);
+            }
+        }
+
+        public boolean hasEdge(T source, T destination) {
+            List<T> neighbors = vertices.get(source);
+            return neighbors != null && neighbors.contains(destination);
+        }
+
+        @Override
+        public void printGraph() {
+            for (T vertex : vertices.keySet()) {
+                List<T> neighbors = vertices.get(vertex);
+                if (neighbors != null) {
+                    System.out.print(vertex + " -> ");
+                    for (T neighbor : neighbors) {
+                        System.out.print(neighbor + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+
+    static class UndirectedGraph<T> extends Graph<T> {
+        public UndirectedGraph() {
+            super();
+        }
+
+        @Override
+        public void addVertex(T vertex) {
+            if (!hasVertex(vertex)) {
+                vertices.put(vertex, new ArrayList<>());
+            }
+        }
+
+        @Override
+        public void addEdge(T sourceVertex, T destinationVertex) {
+            if (hasVertex(sourceVertex) && hasVertex(destinationVertex)) {
+                vertices.get(sourceVertex).add(destinationVertex);
+                vertices.get(destinationVertex).add(sourceVertex); // Since it's an undirected graph
+            }
+        }
+
+        @Override
+        public void printGraph() {
+            for (Map.Entry<T, List<T>> entry : vertices.entrySet()) {
+                T vertex = entry.getKey();
+                List<T> neighbors = entry.getValue();
+                System.out.print(vertex + " -> ");
+                for (T neighbor : neighbors) {
+                    System.out.print(neighbor + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
